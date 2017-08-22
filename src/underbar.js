99ecbf11cff516d874cc5fe8540ca81ce8c1fa9c
tylertruong <tylertruong@gmail.com>
2017-08-22 16:16:52 -0700

    completed underbar.js till extend

--- src/underbar.js
+++ src/underbar.js
@@ -199,12 +199,34 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    
+    if (iterator === undefined) {
+      iterator = _.identity;  
+    }
+    return _.reduce(collection, function (allTrue, item) {  
+      if (allTrue) {
+        if (iterator(item)) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }, true);
+
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (iterator === undefined) {
+      iterator = _.identity;
+    }
+    return !_.every(collection, function (element) {
+      return !iterator(element);
+    });
   };
 
 
@@ -227,6 +249,13 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var result = obj;
+    for (var i = 1; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        result[key] = arguments[i][key];
+      }
+    }
+    return result;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
