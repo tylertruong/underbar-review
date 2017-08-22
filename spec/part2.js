e76b349cb31224b7f82ec9f6e06ce2b35f56cb1b
tylertruong <tylertruong@gmail.com>
2017-08-22 16:16:27 -0700

    fixed part2 tests till _.extend

--- spec/part2.js
+++ spec/part2.js
@@ -56,9 +56,9 @@
       });
 
       it('should return false given an array and a value not in that array', function() {
-        // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var array = [1, 2, 3];
+        var value = 4;
+        expect(_.contains(array, value)).to.be.false;
       });
 
       it('should return true given a object and a value from that object', function() {
@@ -90,8 +90,7 @@
 
       it('fails for a collection of all-falsy values', function() {
         // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        expect(_.every([false, undefined, null, 0, NaN, ''], _.identity)).to.be.false;
       });
 
       it('fails for a collection containing mixed falsy and truthy values', function() {
@@ -147,9 +146,7 @@
       });
 
       it('should fail for a set containing no matching values', function() {
-        // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        expect(_.some([null, 0, '', false], _.identity)).to.be.false;
       });
 
       it('should pass for a collection containing one matching value', function() {
@@ -187,9 +184,11 @@
       });
 
       it('should override properties found on the destination', function() {
-        // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = {b: 'c'};
+        var source = { a: 'b', b: 'd' };
+        var extended = _.extend(destination, source);
+
+        expect(extended.b).to.equal('d');
       });
 
       it('should not override properties not found in the source', function() {
